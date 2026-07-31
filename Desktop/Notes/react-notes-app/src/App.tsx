5cf20b8a998ed70419446fd1ac914b0974c99c19
Erfan Ghesmati <erfanghesmati53@gmail.com>
2026-08-01 02:23:52 +0300

    Implement note deletion

--- Desktop/Notes/react-notes-app/src/App.tsx
+++ Desktop/Notes/react-notes-app/src/App.tsx
@@ -1,18 +1,45 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [note, setNote] = useState([]);
+  const [note, setNote] = useState("");
+  const [notes, setNotes] = useState<string[]>([]);
+
+  function addNote() {
+    setNotes([...notes, note]);
+    setNote("");
+  }
+
+  function deleteNote(indexToDelete: number) {
+    const updatedNotes = notes.filter((_, index) => index !== indexToDelete);
+    setNotes(updatedNotes);
+  }
 
   return (
-    <>
+    <div className="container">
       <h1>React Notes App</h1>
-      <input type="text"
-      placeholder="Enter a note..."
-             value={note}
-             onChange={(event) => setNote(event.target.value)}
+
+      <input
+        type="text"
+        placeholder="Enter a note..."
+        value={note}
+        onChange={(event) => setNote(event.target.value)}
       />
-      <button onClick={() => }>Save</button>
-    </>
+
+      <button onClick={addNote}>Add Note</button>
+
+      <hr />
+
+      {notes.map((item, index) => (
+        <div className="note" key={index}>
+          <p>{item}</p>
+
+          <button onClick={() => deleteNote(index)}>
+            Delete
+          </button>
+        </div>
+      ))}
+    </div>
   );
 }
 
